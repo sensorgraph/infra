9e7baf970a76ffbb57f60805218b47382bcd6c17
Mohamed BASRI <mohamed.basri@outlook.com>
2019-04-29 21:49:11 +0200

    Add HA network and Tags

--- network.tf
+++ network.tf
@@ -4,15 +4,53 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
     cidr_block  = "${var.vpc_cidr}"
+    tags        = "${merge(var.tags, map("Name", "Main"))}"
+}
+
+resource "aws_internet_gateway" "public" {
+    vpc_id  = "${aws_vpc.main.id}"
+    tags    = "${merge(var.tags, map("Name", "Main"))}"
+}
+
+resource "aws_route_table" "public" {
+    vpc_id     = "${aws_vpc.main.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.public.id}"
+    }
+    tags        = "${merge(var.tags, map("Name", "Main"))}"
+}
+
+resource "aws_route_table" "private" {
+    vpc_id      = "${aws_vpc.main.id}"
+    tags        = "${merge(var.tags, map("Name", "Main"))}"
 }
 
 resource "aws_subnet" "public" {
-    vpc_id     = "${aws_vpc.main.id}"
-    cidr_block = "${var.public_cidr}"
-  
+    count               = "${length(var.availability_zones)}"
+    vpc_id              = "${aws_vpc.main.id}"
+    cidr_block          = "${var.public_cidr[count.index]}"
+    availability_zone   = "${var.availability_zones[count.index % length(var.availability_zones)]}"
+    tags                = "${merge(var.tags, map("Name", "Main"))}"
 }
 
 resource "aws_subnet" "private" {
-    vpc_id     = "${aws_vpc.main.id}"
-    cidr_block = "${var.private_cidr}"
+    count               = "${length(var.availability_zones)}"
+    vpc_id              = "${aws_vpc.main.id}"
+    cidr_block          = "${var.private_cidr[count.index]}"
+    availability_zone   = "${var.availability_zones[count.index % length(var.availability_zones)]}"
+    tags                = "${merge(var.tags, map("Name", "Main"))}"
+}
+
+resource "aws_route_table_association" "public" {
+    count           = "${length(var.availability_zones)}"
+    subnet_id       = "${element(aws_subnet.public.*.id, count.index)}"
+    route_table_id  = "${aws_route_table.public.id}"
+}
+
+
+resource "aws_route_table_association" "private" {
+    count           = "${length(var.availability_zones)}"
+    subnet_id       = "${element(aws_subnet.private.*.id, count.index)}"
+    route_table_id  = "${aws_route_table.private.id}"
 }
--- variables.tf
+++ variables.tf
@@ -11,19 +11,34 @@ variable "vpc_cidr" {
 }
 
 variable "private_cidr" {
-    type        = "string"
-    default     = "10.0.0.0/25"
+    type        = "list"
+    default     = ["10.0.0.0/26", "10.0.0.64/26"]
     description = "" 
 }
 
 variable "public_cidr" {
-    type        = "string"
-    default     = "10.0.0.128/25"
+    type        = "list"
+    default     = ["10.0.0.128/26", "10.0.0.192/26"]
     description = "" 
 }
 
+variable "availability_zones" {
+    type        = "list"
+    default     = ["eu-west-3a", "eu-west-3b"]
+    description =  ""
+}
+
+
 variable "tags" {
     type        = "map"
-    default     = {}
+    default     = {
+        Environment = "dev"
+        Application = ""
+        Domain      = ""
+        Phase       = "Build"
+        Technicals  = ""
+        Contact     = "mohamed.basri@outlook.com"
+        Comment     = "Managed by Terraform"
+    }
     description =""
 }
