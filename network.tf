65a841aa06d0081b1594a28351eb1f4112303a8b
Mohamed BASRI <mohamed.basri@outlook.com>
2019-05-25 08:25:20 +0200

    Move to terraform v0.12

--- network.tf
+++ network.tf
@@ -1,46 +1,46 @@
 resource "aws_vpc" "main" {
-    cidr_block  = "${var.vpc_cidr}"
-    tags        = "${merge(var.tags, map("Name", "Main"))}"
+    cidr_block  = var.vpc_cidr
+    tags        = merge(var.tags, map("Name", "Main"))
 }
 
 resource "aws_internet_gateway" "public" {
-    vpc_id  = "${aws_vpc.main.id}"
-    tags    = "${merge(var.tags, map("Name", "Public"))}"
+    vpc_id  = aws_vpc.main.id
+    tags    = merge(var.tags, map("Name", "Public"))
 }
 
 resource "aws_route_table" "public" {
-    vpc_id     = "${aws_vpc.main.id}"
+    vpc_id     = aws_vpc.main.id
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.public.id}"
+        gateway_id = aws_internet_gateway.public.id
     }
-    tags        = "${merge(var.tags, map("Name", "Public"))}"
+    tags        = merge(var.tags, map("Name", "Public"))
 }
 
 resource "aws_route_table" "private" {
-    vpc_id      = "${aws_vpc.main.id}"
-    tags        = "${merge(var.tags, map("Name", "Private"))}"
+    vpc_id      = aws_vpc.main.id
+    tags        = merge(var.tags, map("Name", "Private"))
 }
 
 resource "aws_subnet" "public" {
-    count               = "${length(var.availability_zones)}"
-    vpc_id              = "${aws_vpc.main.id}"
-    cidr_block          = "${var.public_cidr[count.index]}"
-    availability_zone   = "${var.availability_zones[count.index % length(var.availability_zones)]}"
-    tags                = "${merge(var.tags, map("Name", "Public"))}"
+    count               = length(var.availability_zones)
+    vpc_id              = aws_vpc.main.id
+    cidr_block          = var.public_cidr[count.index]
+    availability_zone   = var.availability_zones[count.index % length(var.availability_zones)]
+    tags                = merge(var.tags, map("Name", "Public"))
 }
 
 resource "aws_subnet" "private" {
-    count               = "${length(var.availability_zones)}"
-    vpc_id              = "${aws_vpc.main.id}"
-    cidr_block          = "${var.private_cidr[count.index]}"
-    availability_zone   = "${var.availability_zones[count.index % length(var.availability_zones)]}"
-    tags                = "${merge(var.tags, map("Name", "Private"))}"
+    count               = length(var.availability_zones)
+    vpc_id              = aws_vpc.main.id
+    cidr_block          = var.private_cidr[count.index]
+    availability_zone   = var.availability_zones[count.index % length(var.availability_zones)]
+    tags                = merge(var.tags, map("Name", "Private"))
 }
 
 resource "aws_network_acl" "public" {
-    vpc_id     = "${aws_vpc.main.id}"
-    subnet_ids = ["${aws_subnet.public.*.id}"]
+    vpc_id     = aws_vpc.main.id
+    subnet_ids = aws_subnet.public.*.id
     egress {
         protocol   = "all"
         rule_no    = 200
@@ -58,12 +58,12 @@ resource "aws_network_acl" "public" {
         from_port  = 0
         to_port    = 0
     }
-    tags        = "${merge(var.tags, map("Name", "Public"))}"
+    tags        = merge(var.tags, map("Name", "Public"))
 }
 
 resource "aws_network_acl" "private" {
-    vpc_id     = "${aws_vpc.main.id}"
-    subnet_ids = ["${aws_subnet.private.*.id}"]
+    vpc_id     = aws_vpc.main.id
+    subnet_ids = aws_subnet.private.*.id
     egress {
         protocol   = "all"
         rule_no    = 200
@@ -77,21 +77,21 @@ resource "aws_network_acl" "private" {
         protocol   = "all"
         rule_no    = 200
         action     = "allow"
-        cidr_block = "${var.vpc_cidr}"
+        cidr_block = var.vpc_cidr
         from_port  = 0
         to_port    = 0
     }
-    tags        = "${merge(var.tags, map("Name", "Private"))}"
+    tags        = merge(var.tags, map("Name", "Private"))
 }
 resource "aws_route_table_association" "public" {
-    count           = "${length(var.availability_zones)}"
-    subnet_id       = "${element(aws_subnet.public.*.id, count.index)}"
-    route_table_id  = "${aws_route_table.public.id}"
+    count           = length(var.availability_zones)
+    subnet_id       = element(aws_subnet.public.*.id, count.index)
+    route_table_id  = aws_route_table.public.id
 }
 
 
 resource "aws_route_table_association" "private" {
-    count           = "${length(var.availability_zones)}"
-    subnet_id       = "${element(aws_subnet.private.*.id, count.index)}"
-    route_table_id  = "${aws_route_table.private.id}"
+    count           = length(var.availability_zones)
+    subnet_id       = element(aws_subnet.private.*.id, count.index)
+    route_table_id  = aws_route_table.private.id
 }
