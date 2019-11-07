acdf640fa8b5dcba4740b2e5dcfcc540a819daf7
Mohamed BASRI <mohamed.basri@outlook.com>
2019-11-07 21:08:32 +0100

    Use alias for KMS data

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@ data "aws_route53_zone" "main" {
   zone_id      = "ZC6V3SNTKD66O"
   private_zone = false
 }
-data "aws_kms_key" "s3" {
-  key_id = "arn:aws:kms:eu-west-3:649987087669:key/7f4a8b32-85a5-4962-b472-4e555dd1d13f"
-  #key_id = "aws/s3"}
+
+data "aws_kms_alias" "s3" {
+  name = "alias/aws/s3"
 }
--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "accesslog" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = data.aws_kms_key.s3.arn
+        kms_master_key_id = data.aws_kms_alias.s3.target_key_arn
         sse_algorithm     = "aws:kms"
       }
     }
@@ -32,7 +32,7 @@ resource "aws_s3_bucket" "archivelog" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = data.aws_kms_key.s3.arn
+        kms_master_key_id = data.aws_kms_alias.s3.target_key_arn
         sse_algorithm     = "aws:kms"
       }
     }
